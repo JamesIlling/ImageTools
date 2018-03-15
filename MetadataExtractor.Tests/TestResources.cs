2d6210c8ccdcfd7da368526c879bc9f2cfc30c76
James Illing <James@sherwood37.demon.co.uk>
2018-03-15 20:29:10 +0000

    #1 - AddProcessors
    Added tests for more extractors
    Added data Cannon 4DmkVI from Martin

--- MetadataExtractor.Tests/TestResources.cs
+++ MetadataExtractor.Tests/TestResources.cs
@@ -1,5 +1,6 @@
 ﻿namespace MetadataExtractor.Tests
 {
+    using System.Collections.Generic;
     using System.IO;
     using System.Reflection;
 
@@ -8,6 +9,12 @@
         private const string LightroomJpegPath = "MetadataExtractor.Tests.Resources.Lightroom.jpg";
         private const string AcdSeeJpegPath = "MetadataExtractor.Tests.Resources.ACDSee.jpg";
         private const string D800EJpegPath = "MetadataExtractor.Tests.Resources.D800e.jpg";
+        private const string Eos5DMkIVPath= "MetadataExtractor.Tests.Resources.Eos5DMkIV.jpg";
+
+        public static List<Stream> All()
+        {
+            return new List<Stream>{LightroomJpeg(),AcdSeeJpeg(),Eos5DMkIVJpeg(),D800EJpeg()};
+        }
 
         public static Stream LightroomJpeg()
         {
@@ -23,6 +30,14 @@
 
             assembly.GetManifestResourceNames();
             return assembly.GetManifestResourceStream(AcdSeeJpegPath);
+        }
+
+        public static Stream Eos5DMkIVJpeg()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            assembly.GetManifestResourceNames();
+            return assembly.GetManifestResourceStream(Eos5DMkIVPath);
         }
 
         public static Stream D800EJpeg()
